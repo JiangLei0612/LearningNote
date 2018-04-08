5b2448d98ee4d46d9d497d6259d2102c1a365918
jianglei <jianglei@tiplus.cn>
2018-04-08 16:37:31 +0800

    下载动画

--- app/src/main/java/com/lei/learningnote/MainActivity.java
+++ app/src/main/java/com/lei/learningnote/MainActivity.java
@@ -1,13 +1,62 @@
 package com.lei.learningnote;
 
-import android.support.v7.app.AppCompatActivity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES10.glClear;
+import static android.opengl.GLES10.glClearColor;
+import static android.opengl.GLES10.glViewport;
+import static javax.microedition.khronos.opengles.GL10.GL_COLOR_BUFFER_BIT;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView mGLSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    private void initView() {
+        mGLSurfaceView = new GLSurfaceView(this);
+    }
+
+
+    class FirstRender implements GLSurfaceView.Renderer{
+
+        @Override
+        public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+            glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 gl10, int width, int height) {
+            glViewport(0, 0, width, height);
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl10) {
+            glClear(GL_COLOR_BUFFER_BIT);
+        }
+    }
+
 }
